a797a5e30df5fdd6f9517c00aa144c9087111cf2
sschmidlin <102587195+sschmidlin@users.noreply.github.com>
2024-09-18 12:13:15 +0200

    adding combined figures

--- Experiment combined.R
+++ Experiment combined.R
@@ -237,9 +237,336 @@ plot(m2) +
   scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
 
 
+#####################################################
+#########CREATING PLOTS FOR PUBLICATION##############
+#####################################################
+
+#Adding experiment from May so that plots can be combined##################
+
+
+data1 <- read.csv2(file="Cue interactions 05-2022.csv", sep=",")
+data1 <- select(data, -settled_10hr, -unattached_10hr, -settled_20hr, -unattached_20hr)
+colnames(data1)[5] <- 'Settled'
+colnames(data1)[6] <- 'Unattached'
+data1$Larvae.batch = as.factor(data1$Larvae.batch)
+
+mult_s1 <- rep(1:nrow(data1), data1[, 'Settled'])
+mult_u1 <- rep(1:nrow(data1), data1[, 'Unattached'])
+data_s1 <- data1[mult_s1,]
+data_s1[, 'Settled'] <- 1
+data_s1 <- data_s1[, !(names(data_s1) %in% c('Unattached'))]
+data_u1 <- data1[mult_u1,]
+data_u1[, 'Unattached'] <- 0
+data_u1 <- data_u1[, !(names(data_u1) %in% c('Settled'))]
+colnames(data_u1)[5] <- 'Settled'
+data1 <- rbind(data_s1, data_u1)
+
+data1['Shell'] <- data1['Cue']
+data1['conspecific_cue'] <- data1['Cue']
+data1['predator_cue'] <- data1['Cue']
+data1$Shell <- ifelse(grepl("conspecific shell", data1$Shell), TRUE, FALSE)
+data1$conspecific_cue <- ifelse(grepl("conspecific cue", data1$conspecific_cue), TRUE, FALSE)
+data1$predator_cue <- ifelse(grepl("predator cue", data1$predator_cue), TRUE, FALSE)
+data1$Shell <-sub("TRUE", "Untreated", data1$Shell)
+data1$Shell <-sub("FALSE", "Sterilized", data1$Shell)
+
+data1$predator_cue <-sub("TRUE", "Present", data1$predator_cue)
+data1$predator_cue <-sub("FALSE", "Absent", data1$predator_cue)
+data1$conspecific_cue <-sub("TRUE", "Present", data1$conspecific_cue)
+data1$conspecific_cue <-sub("FALSE", "Absent", data1$conspecific_cue)
+
+
+model1 <-glmer(Settled ~ conspecific_cue + predator_cue + Shell + conspecific_cue:predator_cue + conspecific_cue:Shell + predator_cue:Shell + Larvae.age + (1|Larvae.batch), data=data1, family=binomial)
+#visuals
+m3<-ggpredict(model1, terms = c('conspecific_cue', 'predator_cue'))
+
+m4<-ggpredict(model1, terms = c('Shell','predator_cue'))
+
+m5<-ggpredict(model1, terms = c('Shell', 'conspecific_cue'))
+
+
+#visuals 
+plot(m3) + 
+  labs(x = 'Conspecific Cue (waterbourne)', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Predator Cue")) + 
+  scale_color_manual(values = c("dodgerblue3", "orangered4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+
+plot(m4) + 
+  labs(x = 'Conspecific Shell', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Predator Cue")) + 
+  scale_color_manual(values = c("Khaki2", "orangered4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+
+plot(m5) + 
+  labs(x = 'Conspecific Shell', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Conspecific Cue (waterbourne)")) + 
+  scale_color_manual(values = c("Khaki2","dodgerblue3"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
 
 
 
+####adding august experiment so plots can be combined##########################
+
+data_aug <- read.csv2(file="Cue interactions 08-2022.csv", sep=",")
+colnames(data_aug)[4] <- 'Well_Number'
+colnames(data_aug)[2] <- 'Age'
+colnames(data_aug)[3] <- 'Batch'
+colnames(data_aug)[5] <- 'Cue'
+colnames(data_aug)[6] <- 'Settled'
+colnames(data_aug)[7] <- 'Unattached'
+data_aug[, 'Cue'] <- as.factor(data_aug[, 'Cue'])
+
+
+amult_s <- rep(1:nrow(data_aug), data_aug[, 'Settled'])
+amult_u <- rep(1:nrow(data_aug), data_aug[, 'Unattached'])
+adata_s <- data_aug[amult_s,]
+adata_s[, 'Settled'] <- 1
+adata_s <- adata_s[, !(names(adata_s) %in% c('Unattached'))]
+adata_u <- data_aug[amult_u,]
+adata_u[, 'Unattached'] <- 0
+adata_u <- adata_u[, !(names(adata_u) %in% c('Settled'))]
+colnames(adata_u)[6] <- 'Settled'
+data_aug <- rbind(adata_s, adata_u)
+
+data_aug['Shell'] <- data_aug['Cue']
+data_aug['conspecific_cue'] <- data_aug['Cue']
+data_aug['predator_cue'] <- data_aug['Cue']
+data_aug['biofilm'] <- data_aug['Cue']
+data_aug$Shell <- ifelse(grepl("conspecific shell", data_aug$Shell), TRUE, FALSE)
+data_aug$conspecific_cue <- ifelse(grepl("conspecific cue", data_aug$conspecific_cue), TRUE, FALSE)
+data_aug$predator_cue <- ifelse(grepl("predator cue", data_aug$predator_cue), TRUE, FALSE)
+data_aug$biofilm <- ifelse(grepl("biofilm", data_aug$biofilm), TRUE, FALSE)
+data_aug$biofilm <-sub("TRUE", "Present", data_aug$biofilm)
+data_aug$biofilm <-sub("FALSE", "Absent", data_aug$biofilm)
+data_aug$Shell <-sub("TRUE", "Untreated", data_aug$Shell)
+data_aug$Shell <-sub("FALSE", "Sterilized", data_aug$Shell)
+data_aug$predator_cue <-sub("TRUE", "Present", data_aug$predator_cue)
+data_aug$predator_cue <-sub("FALSE", "Absent", data_aug$predator_cue)
+data_aug$conspecific_cue <-sub("TRUE", "Present", data_aug$conspecific_cue)
+data_aug$conspecific_cue <-sub("FALSE", "Absent", data_aug$conspecific_cue)
+
+model_aug <-glmer(Settled ~ conspecific_cue + Shell + biofilm + predator_cue + conspecific_cue:predator_cue + conspecific_cue:biofilm + Shell:biofilm + Age + (1|Batch), data=data_aug, family=binomial)
+
+m6<-ggpredict(model_aug, terms = c('conspecific_cue', 'predator_cue'))
+
+
+m7<-ggpredict(model_aug, terms = c('conspecific_cue', 'biofilm'))
+
+
+m8<-ggpredict(model_aug, terms = c('Shell', 'conspecific_cue'))
+
+
+m9<-ggpredict(model_aug, terms = c('biofilm','predator_cue'))
+
+
+m10<-ggpredict(model_aug, terms = c('Shell', 'predator_cue'))
+
+
+m11<-ggpredict(model_aug, terms = c('Shell', 'biofilm'))
+
+
+#visuals 
+plot(m6) + 
+  labs(x = 'Conspecific Cue (waterbourne)', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Predator Cue")) + 
+  scale_color_manual(values = c("dodgerblue3", "orangered4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))+
+  geom_errorbar(data = pairwise_emm, aes(x = conspecific_cue, ymin = lower.CL, ymax = upper.CL, color = predator_cue), width = 0.2, position = position_dodge(width = 0.2))
+
+
+plot(m7) + 
+  labs(x = 'Conspecific Cue (waterbourne)', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Biofilm")) + 
+  scale_color_manual(values = c("dodgerblue3", "chartreuse4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+
+plot(m8) + 
+  labs(x = 'Conspecific Shell', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Conspecific Cue (waterbourne)")) + 
+  scale_color_manual(values = c("Khaki2","dodgerblue3"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+plot(m9) + 
+  labs(x = 'Biofilm', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Predator Cue")) + 
+  scale_color_manual(values = c("chartreuse4", "orangered4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+plot(m10) + 
+  labs(x = 'Conspecific Shell', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Predator Cue")) + 
+  scale_color_manual(values = c("Khaki2","orangered4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+plot(m11) + 
+  labs(x = 'Conspecific Shell', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Biofilm")) + 
+  scale_color_manual(values = c("Khaki2","chartreuse4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+
+
+### making one image ############################
+install.packages("grid")
+install.packages("gridExtra")
+library(grid)
+library(gridExtra)
+
+# Convert your ggeffects objects into ggplot objects
+plot_m <- plot(m) + 
+  labs(x = 'Conspecific Cue (waterbourne)', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Predator Cue")) + 
+  scale_color_manual(values = c("dodgerblue3", "orangered4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+plot_m1 <- plot(m1) + 
+  labs(x = 'Conspecific Shell', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Predator Cue")) + 
+  scale_color_manual(values = c("Khaki2", "orangered4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+plot_m2 <- plot(m2) + 
+  labs(x = 'Conspecific Shell', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Conspecific Cue (waterbourne)")) + 
+  scale_color_manual(values = c("Khaki2","dodgerblue3"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+plot_m3 <- plot(m3) + 
+  labs(x = 'Conspecific Cue (waterbourne)', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Predator Cue")) + 
+  scale_color_manual(values = c("dodgerblue3", "orangered4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+plot_m4 <- plot(m4) + 
+  labs(x = 'Conspecific Shell', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Predator Cue")) + 
+  scale_color_manual(values = c("Khaki2", "orangered4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+plot_m5 <- plot(m5) + 
+  labs(x = 'Conspecific Shell', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Conspecific Cue (waterbourne)")) + 
+  scale_color_manual(values = c("Khaki2","dodgerblue3"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+plot_m6 <- plot(m6) + 
+  labs(x = 'Conspecific Cue (waterbourne)', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Predator Cue")) + 
+  scale_color_manual(values = c("dodgerblue3", "orangered4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+  
+
+plot_m7 <- plot(m7) + 
+  labs(x = 'Conspecific Cue (waterbourne)', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Biofilm")) + 
+  scale_color_manual(values = c("dodgerblue3", "chartreuse4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+plot_m8 <- plot(m8) + 
+  labs(x = 'Conspecific Shell', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Conspecific Cue (waterbourne)")) + 
+  scale_color_manual(values = c("Khaki2","dodgerblue3"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+plot_m9 <- plot(m9) + 
+  labs(x = 'Biofilm', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Predator Cue")) + 
+  scale_color_manual(values = c("chartreuse4", "orangered4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+plot_m10 <- plot(m10) + 
+  labs(x = 'Conspecific Shell', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Predator Cue")) + 
+  scale_color_manual(values = c("Khaki2","orangered4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+plot_m11 <- plot(m11) + 
+  labs(x = 'Conspecific Shell', 
+       y= 'Larvae Settled (%)',
+       title = "") +
+  guides(color = guide_legend(title = "Biofilm")) + 
+  scale_color_manual(values = c("Khaki2","chartreuse4"))+
+  theme(axis.text = element_text(size = 12), axis.title = element_text(size = 12))+
+  scale_y_continuous(labels= function(x) paste0(x*100), limits = c(0,1))
+
+# List of ggplot objects
+plot_list <- list(
+  plot_m, plot_m1, plot_m2,
+  plot_m3, plot_m4, plot_m5,
+  plot_m6, plot_m7, plot_m8,
+  plot_m9, plot_m10, plot_m11
+)
+
+# Arrange the plots in a 4x3 grid
+grid.arrange(
+  grobs = plot_list,
+  nrow = 4,
+  ncol = 3
+)
 
 
 
